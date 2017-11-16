7745d9c971cd088fda39437310313397ebd4239e
qjfeeling131 <qjfeeling8994@sina.com>
2017-11-16 16:22:40 +0800

    optimization

--- documentService/urls.py
+++ documentService/urls.py
@@ -1,11 +1,6 @@
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 from documentService import views
 
-
 urlpatterns = [
-    url(r'^api/handleFiles', views.HandleFiles.as_view()),
-    url(r'^api/digitalAsset/',views.digitalAssets.as_view())
+   url(r'^api/handleFiles', views.HandleFiles.as_view()),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- documentService/views.py
+++ documentService/views.py
@@ -5,23 +5,26 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser,FileUploadParser
 from django.http import HttpResponse
 from wsgiref.util import FileWrapper
-from documentService.customValidate import authorization
-from documentService.dbHelper import SQLHelper
-from documentService.selfModels import digitalAsset,restResult
-from documentService.selfSerializer import restResultSerializer
+
 import os, sys
 import requests
 import uuid
 import logging
 import shutil
+import time
+
+from documentService.models import DigitalAsset,RestResult
+from documentService.serializers import DigitalAssetSerializer,ResetResultSerializer
+from documentService.utlities import Authorization
+# Create your views here.
 class HandleFiles(APIView):
     parser_classes=(FileUploadParser,JSONParser )
-    _restReult=restResult(102,'','Upload failed')
+    _restReult=RestResult(102,'','Upload failed')
     logging.basicConfig(filename='handle files.log',format='%(levelname)s:%(asctime)s %(message)s',level=logging.DEBUG)
 
     def createFolder(self,currentFoler,folderName):
         #TODO:Check the current foleer whether exist
-        newFoler=currentFoler+"\\"+folderName
+        newFoler=currentFoler+"/"+folderName
         folderIsExisted=os.path.exists(newFoler)
         if folderIsExisted!=True:
             os.mkdir(newFoler)
@@ -30,12 +33,12 @@ class HandleFiles(APIView):
 
     def post(self,request,format=None):
         tokenHeader=request.META['HTTP_AUTHORIZATION']
-        authorize=authorization(tokenHeader)
+        authorize=Authorization(tokenHeader)
         result= authorize.sendRequest()
-        if result.success !=True:
+        if result is None:
             logging.warning("The current user have not access to call upload file feature")
             self._restReult.message='The current user have not access to call upload file feature'
-            resultSErializer=restResultSerializer(self._restReult)
+            resultSErializer=ResetResultSerializer(self._restReult)
             return Response(resultSErializer.data,status=status.HTTP_400_BAD_REQUEST)       
         file_obj=request.FILES['file']
         #get the current folder.
@@ -48,7 +51,7 @@ class HandleFiles(APIView):
         if fileNameListCount<2:
             logging.error("The file name is invalid")
             self._restReult.message='The file name is invalid'
-            resultSErializer=restResultSerializer(self._restReult)
+            resultSErializer=ResetResultSerializer(self._restReult)
             return Response(resultSErializer.data,status.HTTP_400_BAD_REQUEST)
         contentType=fileNameList[fileNameListCount-1]       
         #TODO:Get file name
@@ -58,9 +61,9 @@ class HandleFiles(APIView):
         while index<fileNameListCount-1:
             fileName=fileName+fileNameList[index]
             index+=1
-        userFoler=self.createFolder(folder, result.userid)
+        userFoler=self.createFolder(folder, result['id'])
         digitalassetFolder=self.createFolder(userFoler,digitalId)
-        path=digitalassetFolder+'\\'+file_obj.name
+        path=digitalassetFolder+'/'+file_obj.name
         try:
             destination=open(path,'wb+')
             for chunk in file_obj.chunks():
@@ -69,35 +72,36 @@ class HandleFiles(APIView):
             destination.close()
             size=size/1024
             #TODO:Create digitalAsset object
-            digitalasset=digitalAsset(digitalId,fileName,contentType,result.userid,'',path,size,'')
-            digitalasset.create()
-            self._restReult.code=101
-            self._restReult.data=digitalasset
-            self._restReult.message="upload digital asset successfully"
-            resultSerializer=restResultSerializer(self._restReult)
-            return Response(resultSerializer.data,status=status.HTTP_201_CREATED)          
+            asset=DigitalAsset(digitalId,fileName,contentType,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),result['id'],time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),result['id'],path,size,'')
+            asset.save()
+            assetSerializer=DigitalAssetSerializer(asset)
+            return Response(assetSerializer.data,status=status.HTTP_201_CREATED)          
         except Exception as ex:
             logging.error(ex)
             self._restReult.message="Application occurs some error, please contact the Admin to check."
             self._restReult.code=104
-            resultSerializer=restResultSerializer(self._restReult)
+            resultSerializer=ResetResultSerializer(self._restReult)
             shutil.rmtree(digitalassetFolder)
             return Response(resultSerializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+    def get_object(self, pk):
+        try:
+            return DigitalAsset.objects.get(pk=pk)
+        except DigitalAsset.DoesNotExist:
+            raise Http404
 
     def get(self,request,format=None):
         try:
             tokenHeader=request.META['HTTP_AUTHORIZATION']
             digitalAssetId=request.query_params.get('id')
-            authorize=authorization(tokenHeader)
+            authorize=Authorization(tokenHeader)
             result= authorize.sendRequest()
-            if result.success !=True:
+            if result is None:
                 logging.warning("The current user have not access to call upload file feature")
                 self._restReult.message='The current user have not access to call upload file feature'
-                resultSErializer=restResultSerializer(self._restReult)
+                resultSErializer=ResetResultSerializer(self._restReult)
                 return Response(resultSErializer.data,status=status.HTTP_400_BAD_REQUEST)       
-            db=SQLHelper()
-            digitalassetDic=db.getDigitalAssetById(digitalAssetId)
-            digitalasset=digitalAsset(digitalassetDic['Id'],digitalassetDic['Name'],digitalassetDic['ContentType'],digitalassetDic['CreateByUserId'],digitalassetDic['ModifyByUserId'],digitalassetDic['Path'],digitalassetDic['Size'],digitalassetDic['Extension'])
+            digitalasset=self.get_object(digitalAssetId)
             file=open(digitalasset.path,'rb')
             response=HttpResponse(FileWrapper(file), content_type='APPLICATION/OCTET-STREAM')
             response['Content-Disposition']='attachment;filename='+digitalasset.name+'.'+digitalasset.contentType
@@ -106,28 +110,5 @@ class HandleFiles(APIView):
         except Exception as e:
             logging.error(e)
             self._restReult.message="The digital asset Id is incorrect, please check your digital id"
-            resultSerializer=restResultSerializer(self._restReult)
+            resultSerializer=ResetResultSerializer(self._restReult)
             return Response(resultSerializer.data,status=HTTP_400_BAD_REQUEST)
-
-class digitalAssets(APIView):
-    parser_classes=(JSONParser,)
-    def get(self,request,format=None):
-        tokenHeader=request.META['HTTP_AUTHORIZATION']
-        authorize=authorization(tokenHeader)
-        result= authorize.sendRequest()
-        if result !=True:
-            return Response(status=status.HTTP_400_BAD_REQUEST)  
-        db=SQLHelper()
-        result=db.getAllUser()
-        db.release();
-        return Response(result)
-    
-    def post(self,request,format=None):
-        tokenHeader=request.META['HTTP_AUTHORIZATION']
-        authorize=authorization(tokenHeader)
-        result= authorize.sendRequest()
-        if result !=True:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        print(request.data)
-
